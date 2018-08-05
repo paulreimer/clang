31e5a213c7097fdda153bb6ea419b4903faf5dda
Paul Reimer <paul@p-rimes.net>
2018-08-04 19:32:58 -0700

    Add CHECK_PARSE_BOOL(DanglingBracket); to FormatTest

--- unittests/Format/FormatTest.cpp
+++ unittests/Format/FormatTest.cpp
@@ -10617,6 +10617,7 @@ TEST_F(FormatTest, ParsesConfigurationBools) {
   CHECK_PARSE_BOOL(BreakStringLiterals);
   CHECK_PARSE_BOOL(CompactNamespaces);
   CHECK_PARSE_BOOL(ConstructorInitializerAllOnOneLineOrOnePerLine);
+  CHECK_PARSE_BOOL(DanglingBracket);
   CHECK_PARSE_BOOL(DanglingParenthesis);
   CHECK_PARSE_BOOL(DerivePointerAlignment);
   CHECK_PARSE_BOOL_FIELD(DerivePointerAlignment, "DerivePointerBinding");
