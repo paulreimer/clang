6812ac2c6b80f7a2e0359c67e0046494b66088c4
Paul Reimer <paul@p-rimes.net>
2018-08-04 18:43:31 -0700

    Use !Left.closesBlockOrBlockTypeList() in BreakBeforeTrailingReturnArrow in mustBreakBefore

--- lib/Format/TokenAnnotator.cpp
+++ lib/Format/TokenAnnotator.cpp
@@ -2948,7 +2948,7 @@ bool TokenAnnotator::mustBreakBefore(const AnnotatedLine &Line,
 
   if (Style.BreakBeforeTrailingReturnArrow &&
       Right.isOneOf(TT_TrailingReturnArrow, TT_LambdaArrow) &&
-      Left.NestingLevel == 1)
+      !Left.closesBlockOrBlockTypeList())
     return true;
 
   if ((Style.Language == FormatStyle::LK_Java ||
