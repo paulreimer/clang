a9ce25cc07a279a499e54ba97d01de760dfc1d57
Paul Reimer <paul@p-rimes.net>
2018-08-04 17:55:47 -0700

    Use Left.Children.empty for BreakBeforeTrailingReturnArrow in mustBreakBefore

--- lib/Format/TokenAnnotator.cpp
+++ lib/Format/TokenAnnotator.cpp
@@ -2948,7 +2948,7 @@ bool TokenAnnotator::mustBreakBefore(const AnnotatedLine &Line,
 
   if (Style.BreakBeforeTrailingReturnArrow &&
       Right.isOneOf(TT_TrailingReturnArrow, TT_LambdaArrow) &&
-      !Line.First->is(tok::r_paren))
+      Left.Children.empty())
     return true;
 
   if ((Style.Language == FormatStyle::LK_Java ||
